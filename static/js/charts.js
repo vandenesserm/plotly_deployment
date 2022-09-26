a4a63e1f21a65fc001761be8cc2a9e627ed8f4b8
vande <rakelmn@gmail.com>
2022-09-25 22:10:50 -0400

    Fix gauge error

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,3 +1,4 @@
+x=[]
 function init() {
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
@@ -62,6 +63,7 @@ function buildCharts(sample) {
     var samples = data.samples; 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var sampleResult = samples.filter(sampleObj => sampleObj.id == sample)
+    console.log("sampleResult")
     console.log(sampleResult)
     //  5. Create a variable that holds the first sample in the array.
     var firstSample = sampleResult[0];
@@ -142,17 +144,25 @@ function buildCharts(sample) {
   // 1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadata = data.metadata;
     var metadataArray = metadata.filter(metaObj => metaObj.id == sample);
+    x = metadataArray
+    console.log("metadataArray")
+    console.log(metadataArray)
   // 2. Create a variable that holds the first sample in the metadata array.
     var firstMeta = metadataArray.slice(0);
+
+    console.log("firstMeta")
+    console.log(firstMeta)
+
   // 3. Create a variable that holds the washing frequency
-    var wfreq = parseFloat(metadataArray.wfreq);
+    var washfreq = parseFloat(metadataArray[0].wfreq);
+
 
     // 4. Create the trace for the gauge chart.
     var gaugeData = [{
       title: {
         text: "<b>Belly Button Washing Frequency</b><br>Scrubs Per Week"
       },
-      value: wfreq,
+      value: washfreq,
       type: "indicator",
       mode: "gauge+number",
       gauge: {
